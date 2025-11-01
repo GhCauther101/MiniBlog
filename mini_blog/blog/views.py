910126e9561890f4925a86c82eb26f807f217978
ggghosthat31 <fridster4140@gmail.com>
2025-11-01 22:48:01 +0300

    update render views, url routes, style templates

--- mini_blog/blog/views.py
+++ mini_blog/blog/views.py
@@ -1,8 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+	{
+		'author': "Authorname",
+		'title': "Blog title",
+		'content': "Somethin happened ... ?",
+		'date_posted': "12 August 2025",
+	},
+	{
+		'author': "Authorname2",
+		'title': "Blog titleeqfdzzz",
+		'content': "Somethin happened ... ?",
+		'date_posted': "12 August 2025",
+	},
+	{
+		'author': "Authorname3",
+		'title': "Blog title222",
+		'content': "Somethin happened ... ?",
+		'date_posted': "12 August 2025",
+	},
+	{
+		'author': "Authorname4",
+		'title': "Blog title 444",
+		'content': "Somethin happened ... ?",
+		'date_posted': "12 August 2025",
+	},
+]
+
 def home(request):
-	return HttpResponse('<h1>Blog home</h1>')
+	context = {
+		'posts': posts
+	}
+	return render(request, 'blog/home.html', context)
 
 def about(request):
-	return HttpResponse('<h1>Blog about</h1>')
+	return render(request, 'blog/about.html', {'title': 'About'})
